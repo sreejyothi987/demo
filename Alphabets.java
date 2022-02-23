6961a81adfb44d6bc3209cbd3d3dc9d1bc031151
sreejyothi987 <97618780+sreejyothi987@users.noreply.github.com>
2022-02-23 14:28:47 +0530

    Alphabets file created

---
+++ Alphabets.java
@@ -0,0 +1,25 @@
+public class Alphabets {
+
+
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				
+				for(int i=5;i>=1;i--) {
+				
+
+					
+					for(int j=i;j<=5;j++) {
+						System.out.print((char)(j+64));
+						
+					}
+					System.out.println(" ");
+					
+				
+					
+				}
+			}
+
+		
+	}
+
